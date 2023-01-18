2a428f324fbc1c2137a8e16da582d30333e87e3c
shreesh_tiwari2 <shreesh@kodnest.com>
2023-01-18 15:27:56 +0530

    Changes on Sidebar

--- src/components/common/layout/root-layout/index.tsx
+++ src/components/common/layout/root-layout/index.tsx
@@ -13,7 +13,160 @@ const RootLayout = (props: any) => {
       {props.children}
       <AppFooter />
       <SideDrawer open={open} setOpen={setOpen}>
-        <p>Some contents...</p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Ea atque molestias sed obcaecati iusto 33
+          tenetur accusantium rem velit inventore quo sint quia sit nihil
+          possimus nam internos esse. Et laudantium repellat aut eius possimus
+          ad galisum modi aut omnis nulla et quia consequatur. Ut facilis illo
+          quo facilis corporis a omnis commodi ea Quis deleniti. Ea sint
+          voluptatibus ea porro voluptatem et explicabo officia. Est minus enim
+          et quas asperiores cum quidem quidem nam voluptatem animi qui
+          perferendis aliquam 33 voluptas ducimus est modi itaque. Ab quaerat
+          unde sit consequuntur maxime ut tempore officia et earum quia et odio
+          officiis aut maiores animi. Et deserunt nemo in voluptatem provident
+          et quos dicta eos beatae delectus. Et sint placeat ut pariatur dolor
+          ut quasi repudiandae.
+        </p>
       </SideDrawer>
     </>
   );
