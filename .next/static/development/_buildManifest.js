9cd864ed98540a47d36c9aad63597b0740ed7194
Hemanth <114749308+Hemanthr3@users.noreply.github.com>
2023-02-08 12:11:04 +0530

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/student-webapp/profile":["static\u002Fchunks\u002Fpages\u002Fstudent-webapp\u002Fprofile.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fstudent-webapp\u002Fprofile"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
